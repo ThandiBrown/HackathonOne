e745bfce0133d697ef599fd49211ddbb3a111510
Thandiwe Brown <thanbrown24@gmail.com>
2017-10-01 15:08:37 -0400

    Add files via upload
    Lastest version of app

--- page.js
+++ page.js
@@ -1,40 +1,70 @@
 
 
 
-
-
-
-
-
-
-
-
 function tabFunction() {
   var nameArray = ["Homer", "Lisa", "Bart", "Marge", "Batman",
   "TheTeletubbies", "Elmo", "Peter", "Spiderman", "Spongebob",
   "Scooby", "Squidward", "AdamSandler", "ThePowerPuffGirls","NedFlanders"];
   var verbArray = ["Eats", "Devours", "Licks", "ChokesOn", "NibblesOn",
   "BreathesIn", "Inhales", "Absorbs", "Ingests", "FeastsUpon",
-  "Demolishes", "Craves", "GetsTurnsAbout", "SleepsOn","DancesOn"];
+  "Demolishes", "Craves", "GetsTurntAbout", "SleepsOn","DancesOn"];
   var objectArray = ["Dirt", "Lemons", "Rainbows", "DangerousNails", "SomeonesSoul",
   "LittleChildren", "MethaneGas", "HairFollicles", "ToeNails", "SustainableEnergy",
   "Chapstick", "Dorthy", "Windex", "Mayonnaise","ABeeHive"];
-  var randomIndex = Math.floor(Math.random() * nameArray.length);
-  var randomElement = nameArray[randomIndex];
-  var random2Index = Math.floor(Math.random() * nameArray.length);
-  var random2Element = verbArray[random2Index];
-  var random3Index = Math.floor(Math.random() * nameArray.length);
-  var random3Element = objectArray[random2Index];
+  var randomIndex = Math.floor(Math.random() * (names.length - 1));
+  var randomElement = names[randomIndex];
+  var random2Index = Math.floor(Math.random() * (action.length - 1));
+  var random2Element = action[random2Index];
+  var random3Index = Math.floor(Math.random() * (random.length - 1));
+  var random3Element = random[random3Index];
   document.getElementById("go").innerHTML = randomElement + "_" + random2Element + "_" + random3Element;
+
 }
 
+function oluFunction(){
+var  names = ["Ricky_ ","Chris_", "Omar_", "Devon_", "Evan_", " Filne_",
+"Drake_","Luchi_","Stevie_","Batman_","Clark_Kent_", "Adele", "Trump", "Obama",
+"BillClinton"];
 
-function oluFunction(){}
+var action = ["slapped", "punched", "kissed", "stabbed", "harassed", "challenged",
+"trapped","fought","smacked","killed","yelled_at","cherished", " cleaned ","headbutted", " kneed ",""];
 
-function derFunction(){}
+var random = ["_a_cheecks", "_a_butt", "_an_elephant", "_a_chicken",
+ "_the_bottom_of_his_shoe","_a_snicker"];
+
+ var randomIndex = Math.floor(Math.random() * (names.length - 1));
+ var randomElement = names[randomIndex];
+ var random2Index = Math.floor(Math.random() * (action.length - 1));
+ var random2Element = action[random2Index];
+ var random3Index = Math.floor(Math.random() * (random.length - 1));
+ var random3Element = random[random3Index];
+document.getElementById("go").innerHTML = randomElement + "_" + random2Element + "_" + random3Element;
+
+}
+
+function derFunction(){
+  var pokemonName1 = ["Gengar", "Dragonite", "Lucario", "Alakazam", "Arcanine",
+                 "Azumarill", "Bellossom", "Charmander", "Charizard",
+                 "Combusken", "Corphish", "Cyndaquil", "Greninja","Gyarados",
+                 "Haunter"];
+  var actionWord = ["Hit","Attacked","Dodged","Defended","Used","Burned",
+                         "Poinoned","Threw", "Punched", "Grabbed", "Punched",
+                         "Kicked", "Faught", "Beat", "Froze", "Paralyzed"];
+  var pokemonName2 = ["Sudowoodo", "Steelix", "Starmie", "Squirtle", "Spiritomb",
+                 "Spoink", "Snorlax", "Slowbro", "Scatterbug",
+                 "Sandslash", "Rayquaza", "Raticate", "Raichu","Plusle",
+                 "Onix"];
+  var randomIndex = Math.floor(Math.random() * (names.length - 1));
+  var randomElement = names[randomIndex];
+  var random2Index = Math.floor(Math.random() * (action.length - 1));
+  var random2Element = action[random2Index];
+  var random3Index = Math.floor(Math.random() * (random.length - 1));
+  var random3Element = random[random3Index];
+ document.getElementById("go").innerHTML = randomElement + "_" + random2Element + "_" + random3Element;
+
+}
 
 var pastNames =[];
-
 
 
 function spaceCheck(){
